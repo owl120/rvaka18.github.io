8fdcaa743780e3aebab4c58caa2a294c6a8873f7
Rishi Vaka <rishikeshvaka@gmail.com>
2016-03-28 19:06:00 -0500

    Added booster.bat

---
+++ booster.bat
@@ -0,0 +1,35 @@
+@echo off
+color 9b
+@echo Don't like this? Next color.
+color bc
+@echo Starting WiFi booster...
+@echo Wait for a few seconds...
+color 9
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 9a
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 76
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 42
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 42
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 21
+ipconfig  /release
+ipconfig /flushdns	
+ipconfig /renew
+color 1
+@echo Done. To restart, just close and reopen this file. To continue, and to view the credits (if you continue there is nothing but credits) use the enter key.
+pause
+start chrome http://rvaka18.github.io/Monstercat.mp3
+start chrome http://rvaka18.github.io
