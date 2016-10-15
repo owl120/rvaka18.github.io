277cfc0fc3574cf8e2a703d900c85e6ee928ab7b
Rishi Vaka <rishikeshvaka@gmail.com>
2016-10-15 11:26:28 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Divided by Zero</title>
+        <style>
+            ::-moz-selection {
+                background: #b3d4fc;
+                text-shadow: none;
+            }
+
+            ::selection {
+                background: #b3d4fc;
+                text-shadow: none;
+            }
+
+            html {
+                padding: 30px 10px;
+                font-size: 20px;
+                line-height: 1.4;
+                color: #333;
+                background: #f0f0f0;
+                -webkit-text-size-adjust: 100%;
+                -ms-text-size-adjust: 100%;
+            }
+
+            html,
+            input {
+                font-family: "Myriad Pro";
+            }
+
+            body {
+                max-width: 700px;
+                width: 500px;
+                padding: 30px 20px 50px;
+                margin: 0 auto;
+            }
+
+            .white-box {
+              webkit-border-radius: 5px;
+              -moz-border-radius: 5px;
+              -ms-border-radius: 5px;
+              -o-border-radius: 5px;
+              border-radius: 5px;
+              -webkit-background-clip: padding;
+              -moz-background-clip: padding;
+              background-clip: padding-box;
+              background-color: #fff;
+              -webkit-box-shadow: 0 1px 1px rgba(1, 1, 1, 0.2);
+              -moz-box-shadow: 0 1px 1px rgba(1, 1, 1, 0.2);
+            }
+
+            h1 {
+                margin: 0 10px;
+                font-weight: 400;
+                text-align: center;
+            }
+
+            h2 {
+                text-align: center;
+                font-weight: 400;
+                font-size: 16pt;
+                margin-top: 0;
+                padding-top: 0;
+            }
+
+            p {
+                margin: 1em 0;
+                font-size: 16pt;
+                text-align: center;
+            }
+
+            ul {
+                padding: 0 0 0 40px;
+                margin: 1em 0;
+            }
+
+            .container {
+                /*width: 400px;*/
+                margin: 0 auto;
+            }
+
+            .container p {
+                margin: 0;
+                padding: 0;
+            }
+
+        </style>
+    </head>
+    <body class="white-box">
+        <div class="container">
+            <h1>404: Not Found</h1>
+            <h2>The page you were trying to view does not exist.</h2>           
+            <p>Maybe you are looking for <a href="http://www.mental-math-trainer.com">Mental Math Trainer Home</a></p>
+            <ul>
+                <li><a href="http://www.troubi.com/kom">Android App: <b>King of Math</b></a></li>
+                <li><a href="http://www.troubi.com/mathtricks">Android App: <b>Math Tricks</b></a></li>
+            </ul>
+        </div>
+    </body>
+</html>
